b6b35f4551ad4632c70a95a5fec1d5a5aa47cc7e
joewiley <marcjohnley@icloud.com>
2024-12-10 17:43:40 +0100

    ajout test typage

--- projet_Tests/type/test.ml
+++ projet_Tests/type/test.ml
@@ -17,6 +17,280 @@ let pathFichiersRat = "../../../../Projets_Tests/type/fichiersRat/"
 (*************)
 (* POINTEURS *)
 (*************)
+let%test_unit "testBaffectation1"= 
+  let _ = compiler (pathFichiersRat^"testBaffectation1.rat") in ()
+
+let%test_unit "testBaffectation2"= 
+  let _ = compiler (pathFichiersRat^"testBaffectation2.rat") in ()
+
+let%test_unit "testBconditionelle1"= 
+  let _ = compiler (pathFichiersRat^"testBconditionnelle1.rat") in ()
+
+let%test_unit "testBconditionelle2"= 
+  let _ = compiler (pathFichiersRat^"testBconditionnelle2.rat") in ()
+
+let%test_unit "testBconditionelle3"= 
+  let _ = compiler (pathFichiersRat^"testBconditionnelle3.rat") in ()
+
+let%test_unit "testBdeclaration1"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration1.rat") in ()
+
+let%test_unit "testBdeclaration2"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration2.rat") in ()
+
+let%test_unit "testBdeclaration3"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration3.rat") in ()
+
+let%test_unit "testBdeclaration4"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration4.rat") in ()
+
+let%test_unit "testBdeclaration5"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration5.rat") in ()
+
+let%test_unit "testBdeclaration6"= 
+  let _ = compiler (pathFichiersRat^"testBdeclaration6.rat") in ()
+
+let%test_unit "testBdenominateur"= 
+  let _ = compiler (pathFichiersRat^"testBdenominateur.rat") in ()
+
+  let%test_unit "testBident"= 
+  let _ = compiler (pathFichiersRat^"testBident.rat") in ()
+
+  let%test_unit "testBident1"= 
+  let _ = compiler (pathFichiersRat^"testBident1.rat") in ()
+
+  let%test_unit "testBident2"= 
+  let _ = compiler (pathFichiersRat^"testBident2.rat") in ()
+
+  let%test_unit "testBnumerateur"= 
+  let _ = compiler (pathFichiersRat^"testBnumerateur.rat") in ()
+
+  let%test_unit "testBnumerateur2"= 
+  let _ = compiler (pathFichiersRat^"testBnumerateur2.rat") in ()
+
+  let%test_unit "testBoperation"= 
+  let _ = compiler (pathFichiersRat^"testBoperation.rat") in ()
+
+  let%test_unit "testBoperation1"= 
+  let _ = compiler (pathFichiersRat^"testBoperation1.rat") in ()
+
+  let%test_unit "testBprint"= 
+  let _ = compiler (pathFichiersRat^"testBprint.rat") in ()
+
+  let%test_unit "testBprint1"= 
+  let _ = compiler (pathFichiersRat^"testBprint1.rat") in ()
+
+  let%test_unit "testBprint2"= 
+  let _ = compiler (pathFichiersRat^"testBprint2.rat") in ()
+
+let%test_unit "testDeclaF1" = 
+try 
+  let _ = compiler (pathFichiersRat^"testDeclaF11.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Rat, Int) -> ()
+
+let%test_unit "testDenominateur" = 
+try 
+  let _ = compiler (pathFichiersRat^"testDenominateur.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int, (Pointeur Int)) -> ()
+
+let%test_unit "testMaffectation" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Rat), Pointeur( (Pointeur Rat))) -> ()
+
+let%test_unit "testMaffectation1" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation1.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int, Rat) -> ()
+
+let%test_unit "testMaffectation3" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation3.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Bool, Rat) -> ()
+
+let%test_unit "testMaffectation4" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation4.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Bool), (Pointeur Rat)) -> ()
+
+let%test_unit "testMaffectation5" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation5.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Rat), Rat) -> ()
+
+let%test_unit "testMaffectation6" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMaffectation6.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur (Pointeur Int)), (Pointeur Bool)) -> ()
+
+let%test_unit "testMappelfonction1" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMappelfonction1.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur (Pointeur Bool)), (Pointeur Bool)) -> ()
+
+let%test_unit "testMconditionnelle" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMconditionnelle.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Equ,Int,Rat) -> ()
+
+let%test_unit "testMconditionnelle2" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMconditionnelle2.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Equ,(Pointeur Int),Bool) -> ()
+
+let%test_unit "testMdeclaPointeurInt3" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaPointeurInt3.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Int),Rat) -> ()
+
+let%test_unit "testMdeclaration1" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration1.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Rat),Int) -> ()
+
+let%test_unit "testMdeclaration2" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration2.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Int),Rat) -> ()
+
+(*a verifier et a confirmer*)
+
+let%test_unit "testMdeclaration3" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration3.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Pointeur((Pointeur Rat)),(Pointeur (Pointeur Int))) -> ()
+
+let%test_unit "testMdeclaration4" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration4.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Pointeur((Pointeur Int)),(Pointeur (Pointeur Rat))) -> ()
+
+let%test_unit "testMdeclaration5" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration5.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Rat,Null) -> ()
+
+let%test_unit "testMdeclaration6" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdeclaration6.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int,Null) -> ()
+
+let%test_unit "testMdenominateur" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMdenominateur.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int,Bool) -> ()
+
+let%test_unit "testMident" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMident.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Rat),(Pointeur Int)) -> ()
+
+let%test_unit "testMident1" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMident1.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Int),(Pointeur Bool)) -> ()
+
+let%test_unit "testMident3" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMident3.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu((Pointeur Bool),(Pointeur Rat)) -> ()
+
+let%test_unit "testMident4" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMident4.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Pointeur((Pointeur Rat)),(Pointeur Rat)) -> ()
+
+let%test_unit "testMnumerateur" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMnumerateur.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int,Bool) -> ()
+
+let%test_unit "testMnumerateur2" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMnumerateur2.rat")
+  in raise ErreurNonDetectee
+with
+| TypeInattendu(Int,(Pointeur Rat)) -> ()
+
+let%test_unit "testMoperation" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMoperation.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Plus,(Pointeur Int),(Pointeur Int)) -> ()
+
+let%test_unit "testMoperation2" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMoperation2.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Inf,(Pointeur Rat),(Pointeur Rat)) -> ()
+
+let%test_unit "testMoperation3" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMoperation3.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Int,Rat) -> ()
+
+let%test_unit "testMoperation4" = 
+try 
+  let _ = compiler (pathFichiersRat^"testMoperation4.rat")
+  in raise ErreurNonDetectee
+with
+| TypeBinaireInattendu(Inf,Pointeur((Pointeur Int)),(Pointeur Int)) -> ()
+
+
+
+
 
 (*
 Vérifier que le type de la variable pointée correspond au type défini.
